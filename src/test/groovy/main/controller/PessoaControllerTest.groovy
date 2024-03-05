193b9c5e1ab284b0bf4a6bd00bfdfb0ce26b1c5f
riquedss <hsantana.sousass@gmail.com>
2024-03-05 01:54:26 -0300

    test: funcionalidade para adicionar candidatos e empresas

---
+++ src/test/groovy/main/controller/PessoaControllerTest.groovy
@@ -0,0 +1,38 @@
+package main.controller
+
+import org.junit.jupiter.api.Test
+import static org.junit.jupiter.api.Assertions.*
+
+class PessoaControllerTest {
+    @Test
+    void inserirCandidatoBd(){
+        Map candidato = [nome: "João Silva",
+         email: "joao.silva@email.com",
+         estado: "São Paulo", cep: "12345678",
+         descricao: "João é um estudante universitário interessado em tecnologia.",
+         cpf: "12345678901",
+         DataNascimento: "05/08/1998"]
+
+        PessoaController controller = new PessoaController("inserir_candidato", candidato)
+
+       Map resultado = controller.action()
+       assertEquals(resultado.status, 200)
+    }
+
+    @Test
+    void inserirEmpresasBd(){
+        Map empresa = [nome: "Empresa B",
+                       email: "contato@empresaB.com",
+                       estado: "Rio de Janeiro",
+                       cep: "87654321",
+                       descricao: "Empresa B é uma empresa de consultoria financeira.",
+                       cnpj: "98765432109876",
+                       pais: "Brasil"]
+
+        PessoaController controller = new PessoaController("inserir_empresa", empresa)
+
+        Map resultado = controller.action()
+        println(resultado)
+        assertEquals(resultado.status, 200)
+    }
+}
